b3670efa7002fd0e4056d2f5e2f1c087587a99c8
sobermaster9000 <awebro207@gmail.com>
2026-06-25 09:51:39 +0800

    update .env.template

--- app/config.py
+++ app/config.py
@@ -7,14 +7,15 @@ class Settings(BaseSettings):
     VERSION: str
 
     DATABASE_URL: str
-    DATABASE_KEY: str
 
     OPENROUTER_MODEL: str
     OPENROUTER_API_ENDPOINT: str
     OPENROUTER_API_KEY: str
+
     HASHING_ITERATIONS: int
     JWT_SIGNATURE: str
     TOKEN_EXPIRY_HOURS: int
+
     OSRM_BASE_URL: str
 
     model_config = SettingsConfigDict(
