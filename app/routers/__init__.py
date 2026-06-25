7064c2cee13647d4b85d4a6d9ff63ea7e25b001a
zachenricotia-design <zachenrico.tia@gmail.com>
2026-06-25 14:24:28 +0800

    Implement announcement schema and endpoints

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,3 +3,4 @@ from .route import router as route_router
 from .report import router as report_router
 from .barangay import router as barangay_router
 from .assignment import router as assignment_router
+from .announcements import router as announcements_router
